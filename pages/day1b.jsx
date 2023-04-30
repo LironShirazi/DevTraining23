d23f5706c86ede103b188f6983937e3b75b7f592
LironKiloma <87006580+LironKiloma@users.noreply.github.com>
2023-04-30 17:51:29 +0300

    hero section of heros!

--- pages/day1b.jsx
+++ pages/day1b.jsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import styles from './day1b.module.css';
 
 export default function Day1b() {
   return (
     <div>
-      <h1>Day 1 Bonus Challenge</h1>
+      <div className={styles.navbarContainer}>
+        <ul className={styles.ulContainer}>
+          <li className={styles.linkContainer}>
+            <Link className={styles.linkStyle} href="/day1">Day #1 </Link>
+          </li>
+        </ul>
+      </div>
+      <h1>Day 1 Bonus Challenge - Heros</h1>
+      <div className={styles.heroContainer}>
+        <div className={styles.imageContainer}>
+          <Image fill src="/2.jpg" alt="hero image" />
+        </div>
+        <p className={styles.contentOfSection}>
+           Welcome to Heros developer - ready for any challenge</p>
+      </div>
     </div>
   );
 }
