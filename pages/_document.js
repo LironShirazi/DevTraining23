f5204cbc7694cb7d07e9114b77e3b652e55744fa
LironKiloma <87006580+LironKiloma@users.noreply.github.com>
2023-04-30 11:02:49 +0300

    First commit - updating some css

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap"
